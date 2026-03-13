0737a751ac6ae2005824a4437d6425839245307f
Kishore-S-Bob <kishore55435@gmail.com>
2026-03-13 09:06:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import MergePage from './pages/MergePage';
 import SplitPage from './pages/SplitPage';
 import CompressPage from './pages/CompressPage';
